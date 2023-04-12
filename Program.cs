8e26735cb5418c7766a452c46737df219281efe5
Gabriel1519 <130614709+Gabriel1519@users.noreply.github.com>
2023-04-12 20:32:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Lado;
+            double a;
+            Console.Write("Insira o Lado do Quadrado: ");
+            Lado = double.Parse(Console.ReadLine());
+
+            a = Math.Pow(Lado, 2);
+
+            Console.WriteLine("A area do Quadrado é {0}.", a);
+        }
+    }
+}
